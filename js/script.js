784455fc6c3c6afcc5e4c270415ec810d8a0e982
Samima-Akter-Tithy <tithysamima789@gmail.com>
2023-02-16 15:02:24 +0600

    Update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 const carousel = document.querySelector(".carousel"),
 firstImg = carousel.querySelectorAll("img")[0];
 arrowIcons = document.querySelectorAll(".wrapper i");
@@ -14,6 +15,51 @@ const showHideIcons = () => {
 arrowIcons.forEach(icon => {
   icon.addEventListener("click", () => {
     carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+    setTimeout(() => showHideIcons(), 60);
+  });
+});
+
+const dragStart = (e) => {
+  isDragStart = true;
+  prevPageX = e.pageX;
+  prevScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+  if(!isDragStart) return;
+  e.preventDefault();
+  carousel.classList.add("dragging");
+  let positionDiff = e.pageX - prevPageX;
+  carousel.scrollLeft = prevScrollLeft - positionDiff;
+}
+
+const dragStop = () => {
+  isDragStart = false;
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mouseup", dragStop);
+
+
+
+
+const carousel = document.querySelector(".carousel"),
+firstBoxs = carousel.querySelectorAll(".boxs")[0];
+arrowIcons = document.querySelectorAll(".wrapper i");
+
+let isDragStart = false, prevPageX, prevScrollLeft;
+let firstBoxsWidth = firstBoxs.clientWidth + 14;
+let scrollWidth = carousel.scrollWidth - carousel.clientWidth;
+
+const showHideIcons = () => {
+  arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+  arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
+}
+
+arrowIcons.forEach(icon => {
+  icon.addEventListener("click", () => {
+    carousel.scrollLeft += icon.id == "left" ? -firstBoxsWidth : firstBoxsWidth;
     setTimeout(() => showHideIcons(), 60);
   });
 });
